434d62bd385fe63dbe6685e5d485b7dabbec376e
Zhenya Petrenko <priestofkarma@gmail.com>
2022-02-02 18:35:05 +0200

    add freelancehunt checksite

--- src/components/Seo.js
+++ src/components/Seo.js
@@ -1,7 +1,7 @@
 import React from "react"
 import { Helmet } from "react-helmet"
 import { useStaticQuery, graphql } from "gatsby"
-import me from '../images/me.jpg'
+import me from '../images/me.jpeg'
 
 const siteData = graphql`
   query {
@@ -16,45 +16,46 @@ const siteData = graphql`
 
 const SEO = ({ pageTitle, pageDescription, pageImage }) => {
 
-  const { site } = useStaticQuery(siteData)
+	const { site } = useStaticQuery(siteData)
 
-  const {
-    title,
-    description,
-    siteUrl,
-  } = site.siteMetadata;
+	const {
+		title,
+		description,
+		siteUrl,
+	} = site.siteMetadata;
 
 
-  const seo = {
-    title: pageTitle,
-    description: pageDescription || description,
-    image: `${siteUrl}${pageImage || me}`,
-  }
+	const seo = {
+		title: pageTitle,
+		description: pageDescription || description,
+		image: `${siteUrl}${pageImage || me}`,
+	}
 
-  const concatTitle = `${seo.title ? `${seo.title} |` : ""} ${title} `;
+	const concatTitle = `${seo.title ? `${seo.title} |` : ""} ${title} `;
 
-  return (
-    <Helmet title={concatTitle}>
-      <html lang="ru" amp />
-      <meta name="description" content={seo.description} />
-      <meta name="image" content={seo.image} />
-      {/* Facebook Card */}
-      <meta property="og:url" content={siteUrl} />
-      <meta property="og:type" content="website" />
-      <meta property="og:title" content={concatTitle} />
-      <meta property="og:description" content={seo.description} />
-      {/* Twitter Card */}
-      {/* images start */}
-      <meta property="og:image" content={seo.image} />
-      <meta property="og:image:height" content="300" />
-      <meta name="twitter:card" content="summary_large_image" />
-      <meta name="twitter:image" content={seo.image} />
-      {/* images end */}
+	return (
+		<Helmet title={concatTitle}>
+			<html lang="ru" amp />
+			<meta name="description" content={seo.description} />
+			<meta name="image" content={seo.image} />
+			{/* Facebook Card */}
+			<meta property="og:url" content={siteUrl} />
+			<meta property="og:type" content="website" />
+			<meta property="og:title" content={concatTitle} />
+			<meta property="og:description" content={seo.description} />
+			{/* Twitter Card */}
+			{/* images start */}
+			<meta property="og:image" content={seo.image} />
+			<meta property="og:image:height" content="300" />
+			<meta name="twitter:card" content="summary_large_image" />
+			<meta name="twitter:image" content={seo.image} />
+			{/* images end */}
 
-      <meta name="twitter:title" content={concatTitle} />
-      <meta name="twitter:description" content={seo.description} />
-    </Helmet>
-  )
+			<meta name="twitter:title" content={concatTitle} />
+			<meta name="twitter:description" content={seo.description} />
+			<meta name='freelancehunt' content='6ef94c07fba183c' />
+		</Helmet>
+	)
 }
 
 export default SEO
